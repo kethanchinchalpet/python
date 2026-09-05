e41e4d2e134f00bb7d1130b8c280aa9d14df2b00
kethan <kethanchinchalpet@gmail.com>
2026-09-05 21:23:49 +0530

    python

---
+++ kethan.first.py
@@ -0,0 +1,17 @@
+#example 1
+
+#print("hello world")
+
+#example 2
+
+# name =(input("enter your name: "))
+
+# print("name: ",name )
+
+# example 3
+
+name = int(input("Enter name: "))
+
+num = int(input("Enter age: "))
+
+print("Name:", name, "Age:", num, sep=",")
---
+++ strg.py
@@ -0,0 +1,109 @@
+# #example 1
+
+# # pi = float(input("pi: "))
+
+# # print("Value of Pi:", pi, sep="")
+
+# #example 2
+
+# a = 10 
+
+# b = 20 
+
+# c = 30
+
+# print("sum of input", a+b+c, sep=":")
+
+# # example 3
+
+# a = input()
+
+# x,y,z = a.split()
+
+# print("x,y,z")
+
+# example 4
+
+#Jhon,25
+
+# inp = input("enter name and nage: ")
+
+# name = inp.split(",")
+
+# n = int(input("enter n: " ))
+
+# print("COuntdown: 5 4 3 2 1 , end = Bloat off!")
+
+# example 5
+
+# 5, 10
+
+# a, b = input("Enter a and b value: ").split(",")
+# a = int(a)
+# b = int(b)
+
+# print("Addition:", a + b, "Subtraction:", a - b, "Multiplacation:", a * b, "Division:", a / b, sep=",")
+
+# example 6
+
+# a = 10>5
+
+# b = 10<5
+
+# c = 10==5
+
+# d = 10!=5
+
+# print("10>5:", a, "10<5:", b, "10==5:", d, "10!=5:", sep=",")
+
+# x,y = input("enter name and age: ").split(",")
+
+# print("name:", x, "age:", y, "years", sep=",")
+
+#example 7 (type 1)
+
+# num1 = 5
+
+# num2 = 10
+
+# print("sun:", num1+num2)
+
+# #     type 2
+
+# num1 = int(input("enter num1: "))
+
+# num2 = int(input("enter num2: "))
+
+# print("sum:", num1+num2)
+
+# example 8 
+ 
+# radius = int(input("enter radius: "))
+
+# a = 3.14*radius**2
+
+# print("Area of circle: ", a) 
+# print("Area of circle: ", a, sep="") 
+
+# print(f"Area of circle: {a}")
+
+#example 9
+
+# a = int(input("enter value of a: "))
+
+# b = int(input("enter value of b: "))
+
+# c = int(input("enter value of c: "))
+
+# root1 = 0
+
+# root2 = 0
+
+# d = (b**2) - 4*a*c
+
+# root1 = (-b + (d**(0.5)))/2*a
+
+# root2 = (-b + (d**(0.5)))/2*a
+
+# print("Roots:", (root1, root2))
+
