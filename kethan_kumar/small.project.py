ae5939a1554ba23f5770d644c629a0169db389a9
kethan <kethanchinchalpet@gmail.com>
2026-09-07 21:24:34 +0530

    strings

---
+++ kethan_kumar/small.project.py
@@ -0,0 +1,63 @@
+# 1 project
+
+# num1 = int(input("enter num1: "))
+
+# num2 = int(input("enter num2: "))
+
+# operatore = input("enter operatore: ")
+
+# if operatore == "+":
+#     print(num1+num2)
+
+# elif operatore == "-":
+#     print(num1-num2)    
+
+# elif operatore == "*":
+#     print(num1*num2)
+
+# elif operatore == "/":
+#     print(num1/num2)
+
+# else:
+#     print ("you are not entered the values")
+    
+# 2 project
+
+# str = "kethan"
+
+# print(str[0])
+# print(str[1])
+# print(str[2])
+# print(str[3])
+# print(str[4])
+# print(str[5])
+
+
+# print(str[-1])
+# print(str[-2])
+# print(str[-3])
+# print(str[-4])
+# print(str[-5])
+# print(str[-6])
+
+# 3 project
+
+# str= "hello world"
+
+# print(str[1])
+
+# print(str[1:4])
+
+# print(str[:4])
+
+# print(str[1:-1])
+
+# ##    [1:], or [1:11] is same
+
+# print(str[1:])
+
+# print(str[::3])
+
+# print(str[1::2])
+
+# print(str[::-1])
